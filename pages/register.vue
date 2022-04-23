e31a217c6735383450e5b5d191a3bb4c02ce85c9
RobieeInt <74244073+RobieeInt@users.noreply.github.com>
2022-04-23 22:45:04 +0700

    register integrasi dengan backend

--- pages/register.vue
+++ pages/register.vue
@@ -5,22 +5,65 @@
         <v-toolbar color="success" dark>Register</v-toolbar>
         <v-card-text>
           <v-form>
-            <v-text-field name="name" label="Name" type="text" />
-            <v-text-field name="email" label="Email" type="email" />
-            <v-text-field name="password" label="Password" type="password" />
             <v-text-field
-              name="repassword"
+              name="fullname"
+              label="Name"
+              type="text"
+              v-model="form.fullname"
+            />
+            <v-text-field
+              name="email"
+              label="Email"
+              type="email"
+              v-model="form.email"
+            />
+            <v-text-field
+              name="password"
+              label="Password"
+              type="password"
+              v-model="form.password"
+            />
+            <v-text-field
+              name="retype_password"
               label="Re-password"
               type="password"
+              v-model="form.retype_password"
             />
           </v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="success" dark>Register</v-btn>
+          <v-btn color="success" @click="onsubmit" dark>Register</v-btn>
         </v-card-actions>
       </v-card>
       <p>Sudah punya Akun? <v-btn to="/login" plain>Login</v-btn></p>
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        fullname: '',
+        email: '',
+        password: '',
+        retype_password: '',
+      },
+    }
+  },
+  methods: {
+    onsubmit() {
+      console.log(this.form)
+      this.$axios
+        .$post('http://localhost:3001/auth/register', this.form)
+        .then((response) => {
+          {
+            console.log(response)
+          }
+        })
+    },
+  },
+}
+</script>
